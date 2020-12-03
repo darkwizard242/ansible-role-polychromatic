240551efd4a42c5bd8c6b5a5fb94a71801a9267e
darkwizard242 <am900820@gmail.com>
2020-12-03 12:28:56 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,18 +6,34 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE = 'polychromatic'
+PACKAGE_BINARY = '/usr/bin/polychromatic-controller'
+
+
 def test_polychromatic_package_installed(host):
+    """
+    Tests if polychromatic is installed.
+    """
     assert host.package("polychromatic").is_installed
 
 
 def test_polychromatic_binary_exists(host):
-    assert host.file('/usr/bin/polychromatic-controller').exists
+    """
+    Tests if polychromatic-controller binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_polychromatic_binary_file(host):
-    assert host.file('/usr/bin/polychromatic-controller').is_file
+    """
+    Tests if polychromatic-controller binary is file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_polychromatic_binary_which(host):
+    """
+    Tests the output to confirm polychromatic-controller's binary location.
+    """
     assert host.check_output('which polychromatic-controller') == \
-      '/usr/bin/polychromatic-controller'
+        PACKAGE_BINARY
